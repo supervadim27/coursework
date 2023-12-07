4c15fc602cc620a46947e4fd8d36909833020920
Вадим <va.arutyunov@gmail.com>
2023-12-07 16:59:36 +0300

    done homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
+        startTask();
+        int number = 10;
+        System.out.println(getSquare(number));
     }
+
+    public static int getSquare(int number) {
+        int sum = number;
+        sum = sum * sum;
+        return sum;
+    }
+    public static void startTask() {
+        System.out.println("Задание из билета №6");
+    }
+
+
 }
